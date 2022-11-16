2874feece4b9aefb770d6b3d1882818b21cf8938
zephyr72836 <zephyr72836@gmail.com>
2022-11-16 17:44:59 +0900

    watch/towatch/best post 진행중

--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -11,6 +11,8 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 # 모델/시리얼라이저
 from .serializers import *
 from .models import *
+from movies.models import *
+from movies.serializers import *
 
 @api_view(['GET'])
 def user_profile(request, user_pk):
@@ -21,6 +23,13 @@ def user_profile(request, user_pk):
 
 @api_view(['POST'])
 def add_to_watch(request, user_pk, movie_pk):
+    user = get_object_or_404(get_user_model(), pk=user_pk)
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    # 프론트에서 새로 추가한 영화를 프로필 데이터에 추가하고 프로필 데이터를 보내줌
+    serializer = ProfileSerializer(user, data=request.data)
+    if serializer.is_valed(raise_exception=True):
+        serializer.save
+
     pass
 
 

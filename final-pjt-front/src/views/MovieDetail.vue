7691e316bc88c6493241ef8ad580f07db63d0769
Bluuubery <zephyr72836@gmail.com>
2022-11-19 19:49:46 +0900

    영화 디테일 페이지 정보 불러오기

--- final-pjt-front/src/views/MovieDetail.vue
+++ final-pjt-front/src/views/MovieDetail.vue
@@ -1,29 +1,62 @@
 <template>
-    <div>
-        hi
+  <div class="MovieDetail">
+    <div class="movie-item">
+      <div
+        class="movie-item__poster"
+        :style="`background-image: url(https://image.tmdb.org/t/p/w400/${movie_detail.poster_path})`">
+        <!-- <img :src="`https://image.tmdb.org/t/p/w400/${movie.fields.poster_path}`" alt=""> -->
+      </div>
+      <div class="movie-item__info">
+        <h3 class="movie-item__info__title">{{ movie_detail.title }}</h3>
+        <div class="movie-item__info__subinfo">
+          <span>{{ movie_detail.released_date | getYear }}・</span
+          ><span>{{ movie_detail.genres[0]["name"] }}・</span
+          ><span>{{ movie_detail.countries[0]["name"] }}</span>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    name: 'FinalPjtMovieDetail',
+  name: "FinalPjtMovieDetail",
 
-    data() {
-        return {
-            movie_pk: this.$route.params.id
-        };
-    },
+  data() {
+    return {
+      movie_pk: this.$route.params.id,
+      movie_detail: {},
+    };
+  },
 
-    created() {
-        
-    },
+  created() {
+    const API_URL = this.$store.state.API_URL;
+    axios({
+      method: "get",
+      url: `${API_URL}/api/movies/${this.movie_pk}/`,
+      headers: {
+        Authorization: `Token ${this.$store.state.token}`,
+      },
+    })
+      .then((res) => {
+        // console.log(res.data)
+        this.movie_detail = res.data;
+        console.log(this.movie_detail);
+      })
+      .catch((err) => console.log(err));
+  },
 
-    methods: {
-        
-    },
+  methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/scss/MovieItem.scss';
 
+.MovieDetail {
+    width: calc(100% - 245px);
+    height: 100vh;
+}
 </style>

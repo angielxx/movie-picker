339bbeccc03e17cf3b30ad2f07e75c4c2cc62253
zephyr72836 <zephyr72836@gmail.com>
2022-11-21 16:02:00 +0900

    review에서 title 제거

--- final-pjt-back/articles/models.py
+++ final-pjt-back/articles/models.py
@@ -6,6 +6,6 @@ from django.conf import settings
 class Review(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    # title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)

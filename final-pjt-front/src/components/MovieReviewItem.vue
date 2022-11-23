1e0bb0ff30d0a25204bfe438797dd5850dea58c0
angielxx <angielxx94@gmail.com>
2022-11-24 01:11:30 +0900

    MovieReviewItem : 영화 코멘트 아이템 프로필 사진 추가

--- final-pjt-front/src/components/MovieReviewItem.vue
+++ final-pjt-front/src/components/MovieReviewItem.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="movieReviewItem">
-    <!-- <h1>title: {{ review.title }}</h1> -->
-    <div class="profile">
+    <div class="profile"
+    :style="`background-image: url(${this.$store.state.API_URL}${this.$store.state.avatar})`"
+    >
 
     </div>
     <div class="content">
       <h2 class="content__username">{{ review.username }}</h2>
       <h2 class="content__content">{{ review.content }}</h2>
     </div>
-    <!-- <h1>{{review.movie}}</h1> -->
   </div>
 </template>
 

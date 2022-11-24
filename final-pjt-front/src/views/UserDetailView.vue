6ca23a008b384189974ec9ddb802748015d3fb05
angielxx <angielxx94@gmail.com>
2022-11-24 11:43:09 +0900

    UserDetail : 상태메세지 수정 완료

--- final-pjt-front/src/views/UserDetailView.vue
+++ final-pjt-front/src/views/UserDetailView.vue
@@ -3,18 +3,41 @@
     <div class="modal hidden">
       <div class="modal__box">
         <div class="modal__box__close" >
-          <svg @click="closeModal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>
+          <svg @click="closeModal('avatar')" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>
         </div>
         <div class="modal__box__content">
           <input type="file" ref="avatarImage" @change="uploadAvatar">
-          <button type="submit" @click.prevent="submitAvatar"> Submit</button>
+          <button type="submit" @click.prevent="closeModal('avatar'), submitAvatar">제출</button>
+        </div>
+      </div>
+    </div>
+    <div class="message-modal hidden">
+      <div class="modal__box">
+        <div class="modal__box__close" >
+          <h2>상태메세지 수정하기</h2>
+          <svg @click="closeModal('message')" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>
+        </div>
+        <div class="modal__box__content">
+          <form action="" @submit.prevent="submitMessage">
+            <textarea cols="30" rows="5"
+            id="message-input"
+            @input="checkLength($event)" maxlength="100"
+            v-model="messageInput"
+            ></textarea>
+            <div>
+              <div>
+                <span id="currentLength">{{ this.message.length }} </span><span> / 100</span>
+              </div>
+              <button type="submit" @click="closeModal('message')">제출</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
     <div class="header">
       <div class="user-info">
         <div class="user-info__img" :style="`background-image: url(${this.$store.state.API_URL}${this.avatar})`">
-          <svg v-if="this.user_pk == this.$store.state.user_pk" @click="showModal" width="484" height="512" viewBox="0 0 484 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg v-if="this.user_pk == this.$store.state.user_pk" @click="showModal('avatar')" width="484" height="512" viewBox="0 0 484 512" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M481.9 166.6C485.1 175.3 482.4 185 475.5 191.2L432.2 230.6C433.3 238.9 433.9 247.4 433.9 256C433.9 264.6 433.3 273.1 432.2 281.4L475.5 320.8C482.4 327 485.1 336.7 481.9 345.4C477.5 357.3 472.2 368.7 466.1 379.7L461.4 387.8C454.8 398.8 447.4 409.2 439.3 419C433.4 426.2 423.6 428.6 414.8 425.8L359.1 408.1C345.7 418.4 330.9 427 315.1 433.5L302.6 490.6C300.6 499.7 293.6 506.9 284.4 508.4C270.6 510.7 256.4 511.9 241.9 511.9C227.4 511.9 213.2 510.7 199.4 508.4C190.2 506.9 183.2 499.7 181.2 490.6L168.7 433.5C152.9 427 138.1 418.4 124.7 408.1L69.0999 425.9C60.2999 428.7 50.4999 426.2 44.5999 419.1C36.4999 409.3 29.0999 398.9 22.4999 387.9L17.7999 379.8C11.6999 368.8 6.39987 357.4 1.99987 345.5C-1.20013 336.8 1.49987 327.1 8.39987 320.9L51.6999 281.5C50.5999 273.1 49.9999 264.6 49.9999 256C49.9999 247.4 50.5999 238.9 51.6999 230.6L8.39987 191.2C1.49987 185 -1.20013 175.3 1.99987 166.6C6.39987 154.7 11.6999 143.3 17.7999 132.3L22.4999 124.2C29.0999 113.2 36.4999 102.8 44.5999 93C50.4999 85.8 60.2999 83.4 69.0999 86.2L124.8 103.9C138.2 93.6 153 85 168.8 78.5L181.3 21.4C183.3 12.3 190.3 5.1 199.5 3.6C213.3 1.2 227.5 0 242 0C256.5 0 270.7 1.2 284.5 3.5C293.7 5 300.7 12.2 302.7 21.3L315.2 78.4C331 84.9 345.8 93.5 359.2 103.8L414.9 86.1C423.7 83.3 433.5 85.8 439.4 92.9C447.5 102.7 454.9 113.1 461.5 124.1L466.2 132.2C472.3 143.2 477.6 154.6 482 166.5L481.9 166.6ZM242 336C286.2 336 322 300.2 322 256C322 211.8 286.2 176 242 176C197.8 176 162 211.8 162 256C162 300.2 197.8 336 242 336Z" fill="url(#paint0_radial_264_147)"/>
             <defs>
             <radialGradient id="paint0_radial_264_147" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(142.256 154.796) rotate(48.5566) scale(580.659 577.426)">
@@ -26,7 +49,7 @@
         </div>
           <div class="user-info__main">
             <h1 class="user-info__main__username">{{ this.username }}
-              <svg v-if="this.user_pk == this.$store.state.user_pk" @click="editMessage" width="266" height="266" viewBox="0 0 266 266" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg v-if="this.user_pk == this.$store.state.user_pk" @click="showModal('message')" width="266" height="266" viewBox="0 0 266 266" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M188.636 9.75106L163.463 34.9218L231.075 102.529L256.248 77.3584C269.251 64.357 269.251 43.2947 256.248 30.2933L235.756 9.75106C222.754 -3.25035 201.69 -3.25035 188.688 9.75106H188.636ZM151.709 46.6751L30.4741 167.952C25.0651 173.361 21.1123 180.07 18.9279 187.402L0.516492 249.965C-0.78375 254.386 0.412473 259.118 3.63707 262.343C6.86168 265.567 11.5946 266.763 15.9634 265.515L78.531 247.105C85.8644 244.921 92.5737 240.968 97.9827 235.56L219.321 114.282L151.709 46.6751Z" fill="url(#paint0_radial_263_144)"/>
               <defs>
               <radialGradient id="paint0_radial_263_144" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(78.0479 80.4371) rotate(46.8745) scale(309.896)">
@@ -36,7 +59,7 @@
               </defs>
               </svg>
             </h1>
-            <h1 v-if="this.message" class="user-info__main__message">{{ this.message }}</h1>
+            <h1 v-if="this.message" class="user-info__main__message" id="message">{{ this.message }}</h1>
             <p v-else class="user-info__main__message">상태 메세지가 없습니다.</p>
           </div>
           <div class="user-info__sub">
@@ -95,10 +118,10 @@ export default {
   data() {
     return {
       user_pk: this.$route.params.userId,
-      username: null,
-      followings: null,
-      followers: null,
-      message: null,
+      username: '',
+      followings: 0,
+      followers: 0,
+      message: '',
       avatar: null,
 
       watched_movies: [],
@@ -111,6 +134,7 @@ export default {
       reviews: [],
 
       selectedFile: null,
+      messageInput: this.message,
 
     };
   },
@@ -127,7 +151,6 @@ export default {
 
   methods: {
     setHeaderImg() {
-      
       if (this.best_movie) {
         document.querySelector('.header').style.backgroundImage = `linear-gradient(to bottom, rgba(20, 18, 23, 1), rgba(20, 18, 23, 0.8)), url(https://image.tmdb.org/t/p/original/${this.best_movie.movie.backdrop_path})`
       } else {
@@ -242,12 +265,14 @@ export default {
       })
       .then((res) => {
         const data = res.data
-        // console.log(data)
         this.followers = data.followers
         this.followings = data.followings
+        if (data.message) this.message = data.message
         this.message = data.message
+        this.messageInput = data.message
         this.username = data.username
         this.avatar = data.avatar
+        console.log('getUserInfo')
       })
       .catch((err) => console.log(err))
     },
@@ -281,20 +306,64 @@ export default {
         .catch((err) => console.log(err))
     },
 
+    // 상메 길이 제한
+    checkLength(event) {
+      const target = event.currentTarget
+      const max = target.getAttribute('maxlength')
+      if (target.value.length > max) {
+        alert('100자 이하로 작성해주세요')
+        target.value = target.value.slice(0, max)
+      }
+      const currentLength = document.querySelector('#currentLength')
+      currentLength.innerText = target.value.length
+    },
+
+    // 상메 수정 제출
+    submitMessage() {
+      const message = this.messageInput
+      axios({
+        method: 'PATCH',
+        url: `${this.$store.state.API_URL}/api/accounts/update_message/`,
+        headers: {
+          Authorization: `Token ${ this.$store.state.token }`
+        },
+        data: {
+          message
+        }
+      })
+      .then(() => {
+        document.querySelector('#message').innerHTML = message
+        // setTimeout(this.getUserInfo(), 500)
+        this.getUserInfo()
+      })
+      // this.$store.dispatch('updateMessage', payload)
+      // this.getUserInfo()
+    },
+
     // 프로필 사진 수정 모달 띄우기
-    showModal() {
-      const modal = document.querySelector('.modal')
-      modal.classList.remove('hidden')
-      //  console.log('modal show')
+    showModal(modalName) {
+      if(modalName === 'avatar') {
+        const modal = document.querySelector('.modal')
+        modal.classList.remove('hidden')
+      } else {
+        const modal = document.querySelector('.message-modal')
+        document.querySelector('textarea').value = this.message
+        modal.classList.remove('hidden')
+      }
     },
     // 프로필 수정 모달 숨기기
-    closeModal() {
-      const modal = document.querySelector('.modal')
-      modal.classList.add('hidden')
-      // console.log('modal hide')
-    },
-    editMessage() {
-      
+    closeModal(modalName) {
+      if (modalName === 'avatar') {
+        const modal = document.querySelector('.modal')
+        modal.classList.add('hidden')
+      } else {
+        const modal = document.querySelector('.message-modal')
+        document.querySelector('#message-input').value = ''
+        const currentLength = document.querySelector('#currentLength')
+        currentLength.innerText = '0'
+        modal.classList.add('hidden')
+      }
+      // console.log('close')
     },
   },
 };
